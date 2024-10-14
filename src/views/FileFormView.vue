d0ce4c540d8180f387638bf3a3dd786d72f5af05
Wojciech Łukowski <wojtek304@gmail.com>
2024-10-14 22:30:38 +0200

    Add FileFormView

--- src/views/FileFormView.vue
+++ src/views/FileFormView.vue
@@ -1,15 +1,34 @@
+<script setup lang="ts">
+import FileForm from "@/components/FileForm.vue";
+</script>
+
 <template>
   <div class="page">
-    <h2>This is a file form page</h2>
+    <h2 class="heading">This is a file form page</h2>
+    <FileForm />
   </div>
 </template>
 
 <style lang="scss">
+.page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+}
+
+.heading {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
 @media (min-width: 1024px) {
   .page {
+    margin-top: 0;
     min-height: 100vh;
-    display: flex;
-    align-items: center;
   }
 }
 </style>
