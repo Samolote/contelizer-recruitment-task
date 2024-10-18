8d589288b333e62893840032b47801a3cd438947
Wojciech Łukowski <wojtek304@gmail.com>
2024-10-19 00:35:21 +0200

    Add basic UserSearch

---
+++ src/components/UserSearch.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const searchModel = defineModel<string>();
+</script>
+
+<template>
+  <form class="form">
+    <div class="form__input-wrapper">
+      <div class="form__input-wrapper">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          class="form__input"
+          required
+          v-model="searchModel"
+        />
+        <label for="search" class="form__input-label">Search for users:</label>
+      </div>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
