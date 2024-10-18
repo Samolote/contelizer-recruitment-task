c7c1440d5bd214a77e127f21a5152d14edd47a33
Wojciech Łukowski <wojtek304@gmail.com>
2024-10-19 00:35:05 +0200

    Add basic UsersEditForm

---
+++ src/components/UserEditForm.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { User } from "@/types/user";
+
+const selectedUser = defineModel<User>();
+
+const emit = defineEmits(["submitEdit", "cancelEdit"]);
+
+const submitEdit = () => {
+  emit("submitEdit");
+};
+
+const cancelEdit = () => {
+  emit("cancelEdit");
+};
+</script>
+
+<template>
+  <div v-if="selectedUser">
+    <h2>Edit User</h2>
+    <form class="form" @submit.prevent="submitEdit">
+      <div class="form__input-wrapper">
+        <input
+          type="text"
+          name="edit-name"
+          id="edit-name"
+          class="form__input"
+          required
+          v-model="selectedUser.name"
+        />
+        <label class="form__input-label" for="edit-name">Name:</label>
+      </div>
+      <div class="form__input-wrapper">
+        <input
+          type="text"
+          name="edit-email"
+          id="edit-email"
+          class="form__input"
+          required
+          v-model="selectedUser.email"
+        />
+        <label for="edit-email" class="form__input-label">Email:</label>
+      </div>
+
+      <button class="form__button">save</button>
+      <button class="form__button" @click="cancelEdit">cancel</button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
