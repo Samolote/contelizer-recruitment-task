d105f91bfd909233afc3ade5fb35800d592b7bff
Wojciech Łukowski <wojtek304@gmail.com>
2024-10-19 09:49:44 +0200

    Update UserList template

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -16,23 +16,73 @@ const startEdit = (user: User) => {
 </script>
 
 <template>
-  <div class="container">
-    <dl>
-      <template v-for="user in props.filteredUsers" :key="user.id">
-        <dt>ID:</dt>
-        <dd>{{ user.id }}</dd>
-        <dt>Name:</dt>
-        <dd>{{ user.name }}</dd>
-        <dt>Email:</dt>
-        <dd>{{ user.email }}</dd>
-        <dt>Gender:</dt>
-        <dd>{{ user.gender }}</dd>
-        <dt>Status:</dt>
-        <dd>{{ user.status }}</dd>
-        <div><button @click="startEdit(user)">edit</button></div>
-      </template>
-    </dl>
-  </div>
+  <ul class="list">
+    <li class="list__item" v-for="user in props.filteredUsers" :key="user.id">
+      <dl class="details-list">
+        <div class="details-list__wrapper">
+          <dt class="details-list__term">ID</dt>
+          <dd class="details-list__description">{{ user.id }}</dd>
+        </div>
+        <div class="details-list__wrapper">
+          <dt class="details-list__term">Name</dt>
+          <dd class="details-list__description">{{ user.name }}</dd>
+        </div>
+        <div class="details-list__wrapper">
+          <dt class="details-list__term">Email</dt>
+          <dd class="details-list__description">{{ user.email }}</dd>
+        </div>
+        <div class="details-list__wrapper">
+          <dt class="details-list__term">Gender</dt>
+          <dd class="details-list__description">{{ user.gender }}</dd>
+        </div>
+        <div class="details-list__wrapper">
+          <dt class="details-list__term">Status</dt>
+          <dd class="details-list__description">{{ user.status }}</dd>
+        </div>
+        <div class="details-list__button-wrapper">
+          <button @click="startEdit(user)">edit</button>
+        </div>
+      </dl>
+    </li>
+  </ul>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.list {
+  padding-left: 20px;
+  border-left: 1px solid var(--color-border);
+  list-style-type: none;
+
+  &__item {
+    padding: 20px;
+    border-bottom: 1px solid var(--color-border);
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+}
+
+.details-list {
+  &__wrapper {
+    display: flex;
+  }
+
+  &__term {
+    font-weight: 500;
+    &::after {
+      content: ":";
+    }
+  }
+
+  &__description {
+    padding-left: 1rem;
+    font-weight: 400;
+  }
+
+  &__button-wrapper {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
