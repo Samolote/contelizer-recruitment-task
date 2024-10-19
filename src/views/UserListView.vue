1481dde273a03e8f7e4a26c481a2ef3d478f1af3
Wojciech Łukowski <wojtek304@gmail.com>
2024-10-19 17:59:57 +0200

    Update UserListView with functionalities

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,15 +1,62 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import UserList from "@/components/UserList.vue";
+import UserSearch from "@/components/UserSearch.vue";
+import UserEditFormDialog from "@/components/UserEditFormDialog.vue";
+import { useUsers } from "@/composables/useUsers";
+import type { User } from "@/types/user";
+
+const { searchQuery, filteredUsers, fetchUsers, isLoading, error, editUser } =
+  useUsers();
+
+const selectedUser = ref<User | null>(null);
+const modalRef = ref<InstanceType<typeof UserEditFormDialog>>();
+
+const startEdit = (user: User) => {
+  selectedUser.value = { ...user };
+  if (modalRef.value) {
+    modalRef.value.show();
+  }
+};
+
+const submitEdit = async () => {
+  if (selectedUser.value) {
+    await editUser(selectedUser.value.id, selectedUser.value);
+    selectedUser.value = null;
+    if (modalRef.value) {
+      modalRef.value.close();
+    }
+  }
+};
+
+const cancelEdit = () => {
+  selectedUser.value = null;
+  if (modalRef.value) {
+    modalRef.value.close();
+  }
+};
+
+onMounted(fetchUsers);
+</script>
+
 <template>
   <div class="page">
-    <h1>This is a page with an User List</h1>
+    <h2 class="page__heading">This is a User List page</h2>
+    <UserSearch v-model="searchQuery" />
+    <p v-if="isLoading">Loading...</p>
+    <p v-if="error">{{ error }}</p>
+    <UserList
+      v-if="!isLoading && !error"
+      :filteredUsers="filteredUsers"
+      @editUser="startEdit"
+    />
+    <UserEditFormDialog
+      ref="modalRef"
+      v-model="selectedUser"
+      @submitEdit="submitEdit"
+      @cancelEdit="cancelEdit"
+    />
   </div>
 </template>
 
-<style lang="scss">
-@media (min-width: 1024px) {
-  .page {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
